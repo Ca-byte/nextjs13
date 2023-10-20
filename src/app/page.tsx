1dcb0c397bbe0c3a1a9e1b247c433ddc95bdb8a4
Caroline Vieira <carolinesanvieira@gmail.com>
2023-10-20 22:55:23 +0200

    streamming ssr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ export default async function Home() {
   await new Promise((resolve) => setTimeout(resolve, 2000))
 
   const response = await fetch('https://api.github.com/users/Ca-byte')
-  const user = await  response.json
+  const user = await response.json()
 
-  return <pre>{JSON.stringify(user)}</pre>
+  return <pre>{JSON.stringify(user, null, 2)}</pre>
   
 }
